84f8152e3237b8e5304252d2c570c96a6822dd2a
Your Name <you@example.com>
2021-05-03 15:03:31 -0400

    somewhat works

--- angular-art/src/app/app.module-signup.ts
+++ angular-art/src/app/app.module-signup.ts
@@ -4,20 +4,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { HttpClientModule } from '@angular/common/http';
+import { SignupComponent } from './app.component-signup';
 
 @NgModule({
   declarations: [
-    AppComponent
+    SignupComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    AppRoutingModule,
-    HttpClientModule
+    AppRoutingModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [SignupComponent]
 })
 export class AppModule { }
