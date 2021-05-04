14d5bc4b0770f5cda52e2011e4bd0a410ff39f6e
Your Name <you@example.com>
2021-05-03 20:09:26 -0400

    login page

---
+++ art-see/src/app/app-routing.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import {AppComponent} from 'src/app/app.component';
+
+const routes: Routes = [
+  { path: 'index', component: AppComponent },
+
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
