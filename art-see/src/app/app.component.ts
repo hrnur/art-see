205642b9653d7c5c9ff1aec7dd98696e400819b0
Your Name <you@example.com>
2021-05-03 22:21:47 -0400

    login page base

--- art-see/src/app/app.component.ts
+++ art-see/src/app/app.component.ts
@@ -1,10 +1,22 @@
 import { Component } from '@angular/core';
+import "@angular/compiler";
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
+
+
 export class AppComponent {
   title = 'art-see';
+
+  constructor(
+    private router: Router,
+) {
+}
+
+  
 }
